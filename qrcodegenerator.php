3a8face4c813e82d93efde792a76f97af71aa712
sathyabaman <sathyabaman@isaithuli.com>
2017-08-01 08:52:53 +0530

    design changes done

--- qrcodegenerator.php
+++ qrcodegenerator.php
@@ -152,7 +152,7 @@ if ($_POST['downloadPDF']) {
                     mywindow.document.write('<style> @page { size: auto;    margin: 0mm;  }</style>');
                     mywindow.document.write('</head><body >');
                     mywindow.document.write(document.getElementById("loadprintin").innerHTML);
-                    mywindow.document.write('<div style="text-align:right; font-size:12px;color:black !important;"> Solution by : surplusdev.com </div>');
+                  //  mywindow.document.write('<div style="text-align:right; font-size:12px; color:black !important;"> Solution by : surplusdev.com </div>');
                     mywindow.document.write('</body></html>');
 
                     mywindow.document.close(); // necessary for IE >= 10
@@ -233,41 +233,192 @@ if ($_POST['downloadPDF']) {
             </table>
             </div>
 
-        <div class="col-lg-4"><img style=" display: block; margin: auto; " id="qrcode_image" src="<?php echo 'http://178.62.43.112/1phpqrcode/'.$PNG_WEB_DIR.basename($filename); ?>"/>  
+        <div class="col-lg-4"><img style=" display: block; margin: auto; " id="qrcode_image" src="<?php echo 'http://localhost/phpqrcode/'.$PNG_WEB_DIR.basename($filename); ?>"/>  
         </div>
 
 
 
         <div id="loadprintin">
-                <style>
-                table, td, th {
-                    border: 1px solid black;
-                    padding: 1.5px;
-                    }
-                table { width:100%;}
-                hr{ margin:0; }
-                </style>
 
+
+
+        <html xmlns="http://www.w3.org/1999/xhtml"><head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Responsive HTML Email Template</title>
+        <style type="text/css">
+
+            @font-face {
+                font-family: digitaltech;
+                src: url(includes/Digital_tech.otf);
+            }
+
+            /*Global Styles*/
+            body { font-family: digitaltech ; margin: 0; padding: 0; min-width: 100%!important;}
+            a { color: #5e96ea; text-decoration: none; font-weight: bold;}
+            img {height: auto;}
+            .content { border: 1px solid #eeeeee; }
+            .logo {font-family: digitaltech; font-size: 30px; font-weight: bold; color: black;}
+            .link a {font-family: digitaltech; font-size: 12px; color: #ffffff;}
+            .subheading {font-size: 14px; color: #cccccc; font-family: digitaltech; font-weight: bold; padding: 0 0 0 0; text-transform: uppercase; letter-spacing: 1px;}
+            .h1 {font-family: digitaltech; font-size: 48px; font-weight: bold; line-height: 56px; color: #ffffff; padding: 0 0 0 0;}
+            .h2 {font-family: digitaltech; font-size: 18px; font-weight: bold; color: #444444; padding: 0 0 0 0; text-transform: uppercase; letter-spacing: 0.5px;}
+            .h3 {font-family: digitaltech; font-size: 24px; font-weight: regular; color: #555555; padding: 0 0 0 0;}
+            .h4 {font-family: digitaltech; font-size: 18px; font-weight: bold; color: #666666; padding: 0 0 0 0;}
+            .paragraph {font-family: digitaltech; font-size: 14px; line-height: 22px; color: #666666; font-weight: 200; padding: 20px 0 0 0;}
+            .listitem {font-family: digitaltech; font-size: 14px; color: #666666; font-weight: 200; padding: 0 0 20px 0;}
+            .listitem-d {font-family: digitaltech; font-size: 25px; color: black; font-weight: 600; padding: 0 0 20px 0;}
+            .smalltext { font-family: digitaltech; font-size: 14px; color: #cccccc; padding: 3px 0 0 0; }
+            .borderbottom {border-bottom: 1px solid #f2eeed;}
+            .num{font-family: digitaltech; font-size: 60px; font-weight: bold; line-height: 56px; color: black  ; padding: 0 0 0 0;}
+
+            
+            
+            /*Media Queries*/
+            @media only screen and (max-width: 651px){
+                .columns{width:100% !important;}
+                .columncontainer{display:block !important; width:100% !important;}
+                .paragraph, .listitem {font-size: 18px;}
+                .link { float: left;}
+            }
+                
+            @media only screen and (min-width: 651px) {
+                .content {width: 650px !important;}
+            }
+            
+        </style>
+    </head>
+    <body bgcolor="#f6f6f6">
+
+        <table width="100%" bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0">
+            <tbody>
+
+            <tr>
+                <td>    
+                    <!--Content Wrapper-->
+                    <table class="content" bgcolor="#ffffff" align="center" cellpadding="0" cellspacing="0" border="0">
+
+                        <tbody>
+<tr>
+                            <td bgcolor="#f1f1f1" style="padding: 30px 20px 20px 30px;">
+                                <table border="0x" cellpadding="0" cellspacing="0" width="100%" class="columns">
+                                    <tbody><tr valign="top">
+                                        <td width="50%" class="columncontainer">
+                                            <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                                <tbody><tr>
+                                                    <td class="logo" >
+                                                        <img src="images/calico.GIF" style="width:45px; height:45px;"> <p style="display:inline;position:relative;bottom:15px;"> CALICO PVT LTD</p>
+                                                    </td>
+                                                </tr>
+                                            </tbody></table>
+                                        </td>
+                                        <td width="50%" valign="middle" class="columncontainer">
+                                            <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                                <tbody><tr>
+                                                    <td align="right" valign="middle" >
+                                                        <table border="0" cellspacing="0" cellpadding="0" class="link">
+                                                            <tbody><tr>
+                                                                <td style="padding: 0px 15px 0px 0px;">
+                                                                    <p class="num"><?php echo $ProductData[0]; ?></p>
+                                                                </td>
+                                                            </tr>
+                                                        </tbody></table>
+                                                    </td>
+                                                </tr>
+                                            </tbody></table>
+                                        </td>
+                                    </tr>
+                                </tbody></table>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td class="borderbottom" style="padding: 25px 30px 0px 30px;">
+                                <table class="data" border="0" cellpadding="0" cellspacing="0" width="620" class="columns">
+                                    <tbody><tr valign="top">
+                                        <td width="50%" class="columncontainer">
+                                            <table border="0" cellspacing="0" cellpadding="0" style="padding: 10px 15px 10px 15px;">
+                                                <tbody><tr>
+                                                    <td class="listitem" valign="top" style="padding: 0 10px 0 0;">Vendor Name</td>
+                                                    <td class="listitem-d"><?php echo $ProductData[1]; ?></td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="listitem" valign="top" style="padding: 0 10px 0 0;">PI Number</td>
+                                                    <td class="listitem-d"><?php echo $ProductData[2]; ?></td>
+                                                    </tr>
+                                                </tr>
+                                                <tr>
+                                                    <td class="listitem" valign="top" style="padding: 0 10px 0 0;">Invoice Number</td>
+                                                    <td class="listitem-d"><?php echo $ProductData[3]; ?></td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="listitem" valign="top" style="padding: 0 10px 0 0;">Item</td>
+                                                    <td class="listitem-d"><?php echo $ProductData[4]; ?></td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="listitem" valign="top" style="padding: 0 10px 0 0;">Color</td>
+                                                    <td class="listitem-d"><?php echo $ProductData[5]; ?></td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="listitem" valign="top" style="padding: 0 10px 0 0;">Style NO</td>
+                                                    <td class="listitem-d"> <?php echo $ProductData[6]; ?></td>
+                                                </tr>
+                                                
+                                            </tbody></table>
+                                        </td>
+                                        <td width="50%" class="columncontainer">
+                                            <table border="0" cellspacing="0" cellpadding="0" style="padding: 10px 15px 10px 15px;">
+                                                <tbody>
+                                                <tr>
+                                                    <td class="listitem" valign="top" style="padding: 0 10px 0 0;">GT</td>
+                                                    <td class="listitem-d"><?php echo $ProductData[7]; ?></td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="listitem" valign="top" style="padding: 0 10px 0 0;">Quantity</td>
+                                                    <td class="listitem-d"><?php echo $ProductData[8]; ?></td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="listitem" valign="top" style="padding: 0 10px 0 0;">Net</td>
+                                                    <td class="listitem-d"><?php echo $ProductData[9]; ?></td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="listitem" valign="top" style="padding: 0 10px 0 0;">Gross</td>
+                                                    <td class="listitem-d"><?php echo $ProductData[10]; ?></td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="listitem" valign="top" style="padding: 0 10px 0 0;">QR</td>
+                                                    <td class="listitem"><img style="width: 100px; height: 100px;" src="<?php echo 'http://localhost/phpqrcode/'.$PNG_WEB_DIR.basename($filename); ?>"></td>
+                                                </tr>
+                                            </tbody></table>
+                                        <p style="float: right; font-family: sans-serif;font-size: 12px; color: black;">Solution By: Surplusdev.com</p>
+                                        </td>
+                                    </tr>
+                                </tbody></table>
+
+                            </td>
+                        </tr>
                         
-                <table>
-                      <tr>
-                        <th style="width: 80%"><img style="display:inline;" src="images/calico.GIF" alt="calico" height="42" width="42"><h1 style="display:inline;position:relative;bottom:15px; left:5px">CALICO (PVT) LTD</h1></th>
-                        <th style="font-size:2em"><h2><?php echo $ProductData[0]; ?></h2></th>
+                        <!--1 Column-->
+                        
 
-                      </tr>
-                      <tr>
-                            <td><strong> Vendor Number : <?php echo $ProductData[1]; ?> </strong>
-                        <hr/><br/> <strong> PI Number:<?php echo $ProductData[2]; ?> </strong>
-                        <hr/><br/> <strong> Invoice Number: <?php echo $ProductData[3]; ?> </strong>
-                        <hr/><br/> <strong> Color : <?php echo $ProductData[4]; ?> </strong>
-                        <hr/><br/> <strong> Style Number: <?php echo $ProductData[5]; ?> </strong>
-                        <hr/><br/> <strong> GT: <?php echo $ProductData[6]; ?> </strong>
-                        <hr/><br/> <strong> Quantity: <?php echo $ProductData[7]; ?> </strong>
-                        <hr/><br/> <strong> Net : <?php echo $ProductData[8]; ?> </strong>
-                        <hr/><br/> <strong> Gross: <?php echo $ProductData[9]; ?> </strong> </td>
-                        <td><img style=" display: block; margin: auto; " id="qrcode_image" src="<?php echo 'http://178.62.43.112/1phpqrcode/'.$PNG_WEB_DIR.basename($filename); ?>"/></td>
-                      </tr>
-                </table>
+                        <!--Footer-->
+                        
+                        
+                    </tbody></table>
+                    
+                    <!--[if (gte mso 9)|(IE)]>
+                          </td>
+                        </tr>
+                      </table>
+                    <![endif]-->
+                </td>
+            </tr>
+        </tbody>
+
+        </table>
+    
+
+</body></html>
+               
         </div>
 
 </div>
